ea9429ef85f34a93289467c778c49ebf331b894d
ChukaCSTD <chuka.ozoadibe@gmail.com>
2023-10-30 16:32:08 -0700

    0x0B. C - malloc, free

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -22,7 +22,7 @@ void ch_free_grid(char **grid, unsigned int height)
  */
 char **strtow(char *str)
 {
-	char **aout;
+	char **bat;
 	unsigned int c, height, i, j, a1;
 
 	if (str == NULL || *str == '\0')

fb0419fdd3326c930d4b17047bcaf88c1d6374e5
ChukaCSTD <chuka.ozoadibe@gmail.com>
2023-11-21 03:38:37 -0800

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,20 +1,24 @@
 #include "lists.h"
 
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
  * print_listint - prints all the elements of a linked list
  * @h: linked list of type listint_t to print
  *
  * Return: number of nodes
  */
+
 size_t print_listint(const listint_t *h)
 {
-	size_t elem = 0;
+	size_t elemm = 0;
 
-	while (h)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		elem++;
 		h = h->next;
-	}
-	return (elem);
+		elem++;
+}
+return (elem);
 }
